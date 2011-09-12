83222b9f5d6c9e2b5f879ca5285be2fed9d22527
Luke Grecki <lukegrecki@gmail.com>
2011-09-11 21:50:20 -0700

    Messed with test

--- test.rb
+++ test.rb
@@ -5,16 +5,20 @@ require 'pp'
 class Tumblr
   include HTTParty
   base_uri 'api.tumblr.com'
-  default_params :api_key => 'KEY_GOES_HERE'
+  default_params :api_key => 'kf8Ox3YsSLmWmO720r8kYFx6Weozf1XCwsbFgac0t2ruRfDI7l'
+  
 end
 
 def gastro
-  Tumblr.get('/v2/blog/gastrogirl.tumblr.com/posts', :query => { :id => 10050252309, :reblog_info => true })['response']['posts'][0]
+  post_0 = Tumblr.get('/v2/blog/gastrogirl.tumblr.com/posts', :query => { :reblog_info => true })['response']['posts'][0]
+  post_id = post_0["id"]
+  
 end
 
 def fufu
-  Tumblr.get('/v2/blog/fufusnomnomz.tumblr.com/posts', :query => { :id => 8104576586, :reblog_info => true, :notes_info => true })['response']['posts'][0]
+  post_0 = Tumblr.get('/v2/blog/fufusnomnomz.tumblr.com/posts', :query => { :reblog_info => true, :notes_info => true })['response']['posts'][0]
+  post_id = post_0["id"]
 end
 
-pp fufu
+pp gastro
 
