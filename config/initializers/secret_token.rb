22909182a69cd6322db3c5ce55ba0bb379ddbade
Jey Kottalam <jey@kottalam.net>
2011-09-11 18:21:41 -0700

    Add Rails 3.1.0 app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tumblr2::Application.config.secret_token = '32c86ee730eeedcd13c819b0daf8787ccd3c0886c9338ace2c323ef6b5fea3928e194b2c10851965f43e9d3f8ac3fccf55f558796d2b5f42908ace2cc6a34b95'
