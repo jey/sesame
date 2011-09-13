5c8f92d51c288a33d91eb68f030b4864339ca9d0
Jey Kottalam <jey@kottalam.net>
2011-09-13 15:21:37 -0700

    fix handling of 'deactivated' blogs, record root post URL, and tweak formatting

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,7 +5,20 @@ class Blog < ActiveRecord::Base
   
   has_many :posts
 
+  def name=(val)
+    self[:name] = self.class.canonical_name(val)
+  end
+
+  def self.canonical_name(name)
+    if name
+      name = name.sub(/.tumblr.com$/, '')
+      name = name.sub(%r{-deactivated\d+-?$}, '')
+    end
+    return name
+  end
+
   def self.find_or_create(attrs)
+    attrs['name'] = canonical_name(attrs['name'])
     result = Blog.find_by_name(attrs['name'])
     result ||= Blog.create!(:name => attrs['name'], :url => attrs['url'])
     return result
